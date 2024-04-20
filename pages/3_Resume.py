b8d34fa8e084a139729146bd64a9534776b854d2
Uche Okechukwu <uche.okechukwu@gmail.com>
2024-04-19 18:11:24 -0600

    fixed browser blocking resume bug

--- pages/3_Resume.py
+++ pages/3_Resume.py
@@ -25,6 +25,9 @@ st.download_button(
 st.divider()
 
 try:
+    pdf_viewer("backend/data/resume.pdf")
+
+except:
     with open("backend/data/resume.pdf", "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
@@ -32,6 +35,5 @@ try:
         <iframe src="data:application/pdf;base64,{base64_pdf}" width="1200mm" height="1000mm"               type="application/pdf"></iframe>
         """
         st.markdown(pdf_display, unsafe_allow_html=True)
-except:
-    pdf_viewer("backend/data/resume.pdf")
+
     
