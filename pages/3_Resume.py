decd32fa0c6f2f687de69230a2fc6abdc62aec1f
Uche Okechukwu <uche.okechukwu@gmail.com>
2024-04-19 18:09:47 -0600

    Updated resume page to fix browsers blocking the pdf

--- pages/3_Resume.py
+++ pages/3_Resume.py
@@ -1,20 +1,37 @@
 import streamlit as st
 import base64
 from sidebar import sidebar
+from streamlit_pdf_viewer import pdf_viewer
+import pandas as pd
+
 
 
 st.set_page_config(page_title="Resume", page_icon="📝")
 sidebar()
 st.title("My Resume")
 
-with open("backend/data/resume.pdf", "rb") as f:
-    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    pdf_display = f"""
-    <iframe src="data:application/pdf;base64,{base64_pdf}" width="1200mm" height="1000mm"               type="application/pdf"></iframe>
-    """
-    st.markdown(pdf_display, unsafe_allow_html=True)
-    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+with open("backend/data/resume.pdf", "rb") as f:
+    pdf_bytes = f.read()
+st.download_button(
+        label="Download resume",
+        data = pdf_bytes,
+        file_name="Uche's Resume.pdf",
+        mime="application/pdf"
+    )
     
-    #    <div style="display: flex; justify-content: center;"></div>
+st.divider()
+
+try:
+    with open("backend/data/resume.pdf", "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+        pdf_display = f"""
+        <iframe src="data:application/pdf;base64,{base64_pdf}" width="1200mm" height="1000mm"               type="application/pdf"></iframe>
+        """
+        st.markdown(pdf_display, unsafe_allow_html=True)
+except:
+    pdf_viewer("backend/data/resume.pdf")
     
